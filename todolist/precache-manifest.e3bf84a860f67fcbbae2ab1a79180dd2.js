eb104f298216edda1945ef94fae91020c82277f0
dsx <ponamarevevgeniy11@gmail.com>
2019-02-03 12:33:07 +0200

    todolist

---
+++ todolist/precache-manifest.e3bf84a860f67fcbbae2ab1a79180dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cadd4ac4c9a27ca520d",
+    "url": "/static/css/main.b22dfcf0.chunk.css"
+  },
+  {
+    "revision": "1cadd4ac4c9a27ca520d",
+    "url": "/static/js/main.1cadd4ac.chunk.js"
+  },
+  {
+    "revision": "fadf4c4da9e9309beecf",
+    "url": "/static/js/1.fadf4c4d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d05982646985c3224594f199f7d73cf3",
+    "url": "/index.html"
+  }
+];
