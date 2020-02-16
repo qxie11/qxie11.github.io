2e31d52b0346a8070d72ed94b873bff520146fa3
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2020-02-16 19:58:33 +0200

    test-game done

--- test-task/src/store/map/mapActions.js
+++
@@ -1,7 +0,0 @@
-export const SAVE_LOCATION = "AUTH_CHANGE_EMAIL_TEXT";
-
-export const saveLocation = (location) => ({
-    type: SAVE_LOCATION,
-    payload: location
-});
-
--- test-task/src/store/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { mapReducer } from './map/mapReducers';
-
-export default combineReducers({
-   map: mapReducer
-})
