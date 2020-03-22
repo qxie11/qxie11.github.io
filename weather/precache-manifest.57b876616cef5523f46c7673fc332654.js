5a9eb2c574b92ba2dd6d7fe004937314c0638d54
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2020-03-22 17:12:03 +0200

    weather unadapted

---
+++ weather/precache-manifest.57b876616cef5523f46c7673fc332654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cccf1d000a5fc89448437aecac93252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15788121ce1345dee2c0",
+    "url": "/static/css/main.725fb9ff.chunk.css"
+  },
+  {
+    "revision": "00132b89bc1a0323bec5",
+    "url": "/static/js/2.cfc274a6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.cfc274a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15788121ce1345dee2c0",
+    "url": "/static/js/main.b6e60c0c.chunk.js"
+  },
+  {
+    "revision": "a008a97a3a24b891c894",
+    "url": "/static/js/runtime-main.576133a0.js"
+  },
+  {
+    "revision": "14b2492707fd955a63c628dc6f0ad7a7",
+    "url": "/static/media/loading.14b24927.png"
+  }
+]);
