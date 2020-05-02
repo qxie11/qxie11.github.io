451123796b46ba18d3e759b14b1818ad13d0eff3
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2020-05-02 17:22:02 +0300

    covid

---
+++ covid-monitoring/precache-manifest.8cd203cb328814ce7654a6196576a19f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb3f2b702558e67154d00d8f47ae738",
+    "url": "/covid-monitoring/index.html"
+  },
+  {
+    "revision": "b62fa76a506d2dc9e35f",
+    "url": "/covid-monitoring/static/css/main.2551f77d.chunk.css"
+  },
+  {
+    "revision": "e6c95a438bdc964bf78b",
+    "url": "/covid-monitoring/static/js/2.cea84c6a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-monitoring/static/js/2.cea84c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62fa76a506d2dc9e35f",
+    "url": "/covid-monitoring/static/js/main.ea8f5723.chunk.js"
+  },
+  {
+    "revision": "7f278635a968c43356c7",
+    "url": "/covid-monitoring/static/js/runtime-main.7543a624.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-monitoring/static/media/virus.d7265326.png"
+  }
+]);
