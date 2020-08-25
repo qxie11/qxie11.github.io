6c6940029b660a1d91eb732c4188e5c326335b67
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2020-08-25 15:36:50 +0300

    crypto add

---
+++ crypto-app/precache-manifest.12878d8b9a540ea6379e908274e39de1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84106fc59193cb612443283d0667fb6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9542aa4d3d28be441154",
+    "url": "/static/css/main.522edbff.chunk.css"
+  },
+  {
+    "revision": "045798d5b1c83a75aa20",
+    "url": "/static/js/2.4fda1826.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4fda1826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9542aa4d3d28be441154",
+    "url": "/static/js/main.d9c58d57.chunk.js"
+  },
+  {
+    "revision": "d049e954b1c74f29178c",
+    "url": "/static/js/runtime-main.9c7c50c1.js"
+  },
+  {
+    "revision": "f10f88dab15ff22e57be2797382fc0e9",
+    "url": "/static/media/loading.f10f88da.gif"
+  }
+]);
