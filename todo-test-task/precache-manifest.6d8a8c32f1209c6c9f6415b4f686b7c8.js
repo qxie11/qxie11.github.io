df2995ee29872df7ab36c30d4aa926156c7278fc
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2020-09-16 23:01:00 +0300

    todo test task

---
+++ todo-test-task/precache-manifest.6d8a8c32f1209c6c9f6415b4f686b7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce56f952b722e6cac1f1a93f03d31fd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8848317fee79237752d0",
+    "url": "/static/css/main.9fcc1c5e.chunk.css"
+  },
+  {
+    "revision": "a49ab0d990d85c7159f9",
+    "url": "/static/js/2.c014ac72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c014ac72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8848317fee79237752d0",
+    "url": "/static/js/main.721b0ae8.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
